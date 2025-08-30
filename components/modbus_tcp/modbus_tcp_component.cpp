eeb2dd9871528c8ab5aa80aaf3cc000f302f0523
Gucioo <tomasz.baryla@gmail.com>
2025-08-31 00:03:45 +0100

    Update modbus_tcp_component.cpp

--- components/modbus_tcp/modbus_tcp_component.cpp
+++ components/modbus_tcp/modbus_tcp_component.cpp
@@ -4,14 +4,14 @@
 namespace esphome {
 namespace modbus_tcp {
 
-static const char *const TAG = "modbus_tcp";
-
 // Since the original header contains the complete implementation,
 // this file is mainly for ESPHome compatibility and any additional methods
 // that might be needed for the external component structure.
 
 // The ModbusTCPSensor class is already fully implemented in the header file
 // as a header-only implementation, which is valid C++.
+
+// TAG is already defined in the header file, so we don't redefine it here.
 
 // If you need to add any additional implementation that wasn't in the header,
 // you can add it here.
