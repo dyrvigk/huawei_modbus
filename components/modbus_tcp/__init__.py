0a593727d7effa433e88acab6674082166bf5fc4
Gucioo <tomasz.baryla@gmail.com>
2025-08-30 23:46:58 +0100

    Create __init__.py

---
+++ components/modbus_tcp/__init__.py
@@ -0,0 +1,16 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+# Define the namespace and component class
+modbus_tcp_ns = cg.esphome_ns.namespace("modbus_tcp")
+ModbusTCPComponent = modbus_tcp_ns.class_("ModbusTCPComponent", cg.Component)
+
+# Configuration schema for the main component
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(ModbusTCPComponent),
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
