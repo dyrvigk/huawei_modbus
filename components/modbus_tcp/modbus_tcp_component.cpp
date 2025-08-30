a106cae8e87c39f929b3dcca8e016813f8c367f7
Gucioo <tomasz.baryla@gmail.com>
2025-08-30 23:47:54 +0100

    Create modbus_tcp_component.cpp

---
+++ components/modbus_tcp/modbus_tcp_component.cpp
@@ -0,0 +1,20 @@
+#include "modbus_tcp_component.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace modbus_tcp {
+
+static const char *const TAG = "modbus_tcp";
+
+// Since the original header contains the complete implementation,
+// this file is mainly for ESPHome compatibility and any additional methods
+// that might be needed for the external component structure.
+
+// The ModbusTCPSensor class is already fully implemented in the header file
+// as a header-only implementation, which is valid C++.
+
+// If you need to add any additional implementation that wasn't in the header,
+// you can add it here.
+
+}  // namespace modbus_tcp
+}  // namespace esphome
