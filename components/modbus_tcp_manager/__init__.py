b7f9d3e3b7f26a06a8613fc05963da243650c339
Gucioo <tomasz.baryla@gmail.com>
2025-08-31 14:02:24 +0100

    Update __init__.py

--- components/modbus_tcp_manager/__init__.py
+++ components/modbus_tcp_manager/__init__.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
+from esphome.core import CORE
 
 # Configuration constants
 CONF_HOST = "host"
@@ -35,29 +36,6 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_SAFE_MODE_REGISTERS, default=[]): cv.All(cv.ensure_list(SAFE_MODE_REGISTER_SCHEMA)),
 }).extend(cv.COMPONENT_SCHEMA)
 
-# Action schemas for writing
-ModbusTCPWriteAction = modbus_tcp_ns.class_("ModbusTCPWriteAction", cg.Action)
-ModbusTCPWriteMultipleAction = modbus_tcp_ns.class_("ModbusTCPWriteMultipleAction", cg.Action)
-
-CONF_REGISTER = "register"
-CONF_VALUE = "value"
-CONF_REGISTERS = "registers"
-CONF_VALUES = "values"
-
-# Write single register action
-MODBUS_TCP_WRITE_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.use_id(ModbusTCPManager),
-    cv.Required(CONF_REGISTER): cv.templatable(cv.positive_int),
-    cv.Required(CONF_VALUE): cv.templatable(cv.int_range(min=-32768, max=32767)),
-})
-
-# Write multiple registers action  
-MODBUS_TCP_WRITE_MULTIPLE_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.use_id(ModbusTCPManager),
-    cv.Required(CONF_REGISTER): cv.templatable(cv.positive_int),
-    cv.Required(CONF_VALUES): cv.All(cv.ensure_list(cv.templatable(cv.int_range(min=-32768, max=32767))), cv.Length(min=1, max=123)),
-})
-
 async def to_code(config):
     var = cg.new_Pvariable(
         config[CONF_ID],
@@ -76,32 +54,3 @@ async def to_code(config):
         cg.add(var.add_safe_mode_register(safe_reg["register"], safe_reg["value"]))
     
     await cg.register_component(var, config)
-
-@cg.register_action("modbus_tcp.write_register", ModbusTCPWriteAction, MODBUS_TCP_WRITE_SCHEMA)
-async def modbus_tcp_write_to_code(config, action_id, template_arg, args):
-    parent = await cg.get_variable(config[CONF_ID])
-    var = cg.new_Pvariable(action_id, template_arg, parent)
-    
-    register_template = await cg.templatable(config[CONF_REGISTER], args, cg.uint16)
-    cg.add(var.set_register(register_template))
-    
-    value_template = await cg.templatable(config[CONF_VALUE], args, cg.int16)
-    cg.add(var.set_value(value_template))
-    
-    return var
-
-@cg.register_action("modbus_tcp.write_multiple_registers", ModbusTCPWriteMultipleAction, MODBUS_TCP_WRITE_MULTIPLE_SCHEMA)
-async def modbus_tcp_write_multiple_to_code(config, action_id, template_arg, args):
-    parent = await cg.get_variable(config[CONF_ID])
-    var = cg.new_Pvariable(action_id, template_arg, parent)
-    
-    register_template = await cg.templatable(config[CONF_REGISTER], args, cg.uint16)
-    cg.add(var.set_register(register_template))
-    
-    values = []
-    for value_config in config[CONF_VALUES]:
-        value_template = await cg.templatable(value_config, args, cg.int16)
-        values.append(value_template)
-    cg.add(var.set_values(values))
-    
-    return var
